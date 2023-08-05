9991c51aad88f756e8ab84a2265d18aeb85b2abf
Gabrielaa217 <jhoana.alvarado@ustabuca.edu.co>
2023-08-04 21:49:23 -0500

    CAMBIOS CLASE
    SE REALIZAN CAMBIOS SEGÚN ACTIVIDAD

--- ISASCE/isasce front/js/limpiarCampos.js
+++ ISASCE/isasce front/js/limpiarCampos.js
@@ -1,12 +1,4 @@
-function limpiarCamposTorres(){
-    $("#input_valor_torres").val("");
-}
-function limpiarCamposPedroza(){
-    $("#input_valor_pedroza").val("");
-}
+
 function limpiarCamposJhoana(){
-    $("#input_valor_jhoana").val("");
-}
-function limpiarCamposHector(){
-    $("#input_valor_hector").val("");
+    $("#input_color").val("");
 }
