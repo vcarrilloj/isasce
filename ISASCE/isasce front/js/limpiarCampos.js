f8de73e78a92f6850c2ad93520005bd036c5cc1b
RicardoMedina2 <mitzrandir29@gmail.com>
2023-08-09 15:59:29 -0500

    COMMIT limpiar_campos corregido
    Se revisa, corrige y actualiza el archivo de limpiarCampos.js

--- ISASCE/isasce front/js/limpiarCampos.js
+++ ISASCE/isasce front/js/limpiarCampos.js
@@ -6,7 +6,23 @@ function limpiarCamposJhoana(){
 function limpiarCamposHector(){
     $("#input_valor_hector").val("");
 }
+
 function limpiarCamposCarrillo(){
     $("#limpiarCamposCarrillo").val("");
 }
 
+function limpiarCamposPedroza(){
+    $("#input_valor_pedroza").val("");
+}
+
+function limpiarCamposTorres(){
+    $("#input_valor_torres").val("");
+}
+
+function limpiarCamposHector(){
+    $("#input_valor_hector").val("");
+}
+/**función limopiar campos para Eudrey */
+/* function limpiarCamposTorres(){
+    $("#input_valor_torres").val("");
+} */
